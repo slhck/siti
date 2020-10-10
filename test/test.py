3d3e9a0a3772d3999a9d9e8b32408b1cda60acb2
Werner Robitza <werner.robitza@gmail.com>
2020-10-10 20:09:11 +0200

    fix tests

--- test/test.py
+++ test/test.py
@@ -3,7 +3,7 @@
 import sys
 import os
 
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 
 import siti.__main__ as siti
 
