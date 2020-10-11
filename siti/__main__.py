0b57db59f043f42268a1338c6356efa886d18057
Werner Robitza <werner.robitza@gmail.com>
2020-10-11 14:45:56 +0200

    add support for YUV

--- siti/__main__.py
+++ siti/__main__.py
@@ -13,6 +13,7 @@
 # Outputs SI/TI statistics as JSON to stderr or to a file.
 
 import argparse
+import pdb
 import av
 import json
 import numpy as np
@@ -60,7 +61,76 @@ def calculate_ti(frame_data, previous_frame_data):
         return (frame_data - previous_frame_data).std()
 
 
-def calculate_si_ti(input_file, quiet=False, num_frames=0):
+def read_yuv(input_file: str, width: int, height: int):
+    """
+    Read a YUV file and yield the per-frame Y data
+    """
+    # get the number of frames again
+    num_frames = get_num_frames(input_file, width=width, height=height)
+
+    if num_frames is None:
+        raise RuntimeError("Could not determine number of frames!")
+
+    with open(input_file, "rb") as in_f:
+        for _ in range(num_frames):
+            y_data = (
+                np.frombuffer(in_f.read((width * height)), dtype=np.uint8)
+                .reshape((height, width))
+                .astype("float32")
+            )
+            u_data = (
+                np.frombuffer(in_f.read(((width // 2) * (height // 2))), dtype=np.uint8)
+                .reshape((height // 2, width // 2))
+                .astype("float32")
+            )
+            v_data = (
+                np.frombuffer(in_f.read(((width // 2) * (height // 2))), dtype=np.uint8)
+                .reshape((height // 2, width // 2))
+                .astype("float32")
+            )
+            # FIXME: is that correct? or do we need a translation function
+            yield y_data
+
+
+def read_file(input_file: str):
+    """
+    Read a regular file and yield the per-frame grey data via PyAV
+    """
+    container = av.open(input_file)
+
+    if not len(container.streams.video):
+        raise RuntimeError("No video streams found!")
+
+    for frame in container.decode(video=0):
+        frame_data = (
+            frame.to_ndarray(format="gray")
+            .reshape(frame.height, frame.width)
+            .astype("float32")
+        )
+        yield frame_data
+
+
+def get_num_frames(input_file: str, width=None, height=None):
+    """
+    Use PyAV or simple math to get the number of frames in a container
+    """
+    if input_file.endswith(".yuv"):
+        file_size = os.path.getsize(input_file)
+        # FIXME: this is only valid for YUV420
+        num_frames = file_size // (width * height * 3 // 2)
+    else:
+        container = av.open(input_file)
+        num_frames = container.streams.video[0].frames
+        if num_frames == 0:
+            num_frames = None
+            print(
+                "Warning: frame count could not be detected from stream",
+                file=sys.stderr,
+            )
+    return num_frames
+
+
+def calculate_si_ti(input_file: str, quiet=False, num_frames=0, width=0, height=0):
     """
     Calculate SI and TI from an input file
 
@@ -70,34 +140,33 @@ def calculate_si_ti(input_file, quiet=False, num_frames=0):
     Keyword Arguments:
         quiet {bool} -- do not output tqdm progress bar (default: {False})
         num_frames {int} -- number of frames to parse (default: {0})
-        magnitude {bool} -- use alternative calculation for SI magnitude (default: {True})
+        width {int} -- frame width for YUV files (default: None)
+        height {int} -- frame height for YUV files (default: None)
 
     Returns:
         - [si_values], [ti_values], frame count
     """
     si_values = []
     ti_values = [0.0]
-
-    container = av.open(input_file)
-
     previous_frame_data = None
 
-    # get video
-    if not len(container.streams.video):
-        raise RuntimeError("No video streams found!")
+    if input_file.endswith(".yuv"):
+        print(
+            "Warning: Reading YUV files may produce values different from what you would get if you analyzed a muxed (e.g. MP4) file.. See https://github.com/slhck/siti/issues/4 for more info.",
+            file=sys.stderr,
+        )
+        kwargs = {"width": width, "height": height}
+        iterator_fun = read_yuv
+    else:
+        kwargs = {}
+        iterator_fun = read_file
 
-    # initialize progress
-    if not num_frames:
-        num_frames = container.streams.video[0].frames
-        if num_frames == 0:
-            num_frames = None
-            print("Warning: frame count could not be detected from stream", file=sys.stderr)
+    num_frames = get_num_frames(input_file, **kwargs)
+
     t = tqdm(total=num_frames, disable=quiet, file=sys.stderr)
 
     current_frame = 0
-    for frame in container.decode(video=0):
-        frame_data = frame.to_ndarray(format="gray").reshape(frame.height, frame.width).astype("float32")
-
+    for frame_data in iterator_fun(input_file, **kwargs):
         si = calculate_si(frame_data)
         si_values.append(si)
 
@@ -117,38 +186,42 @@ def calculate_si_ti(input_file, quiet=False, num_frames=0):
 
 
 def main():
-    parser = argparse.ArgumentParser(prog="siti", description="siti v{}".format(version))
-    parser.add_argument(
-        "input",
-        help="input file"
+    parser = argparse.ArgumentParser(
+        prog="siti", description="siti v{}".format(version)
     )
+    parser.add_argument("input", help="input file", type=str)
     parser.add_argument(
-        "-of", "--output-format",
+        "-of",
+        "--output-format",
         type=str,
         choices=["json", "csv"],
         default="json",
-        help="output format"
+        help="output format",
     )
     parser.add_argument(
-        "-q", "--quiet",
-        help="do not show progress bar",
-        action="store_true"
+        "-q", "--quiet", help="do not show progress bar", action="store_true"
     )
     parser.add_argument(
-        "-n", "--num-frames",
-        help="number of frames to calculate",
-        type=int
+        "-n", "--num-frames", help="number of frames to calculate", type=int
     )
+    parser.add_argument("--width", help="frame width (for YUV files)", type=int)
+    parser.add_argument("--height", help="frame height (for YUV files)", type=int)
     cli_args = parser.parse_args()
 
     if (cli_args.num_frames is not None) and cli_args.num_frames < 2:
         print("Need at least -n 2!", file=sys.stderr)
         sys.exit(1)
 
+    if cli_args.input.endswith(".yuv") and not (cli_args.width and cli_args.height):
+        print("Need to specify --width and --height for YUV!", file=sys.stderr)
+        sys.exit(1)
+
     si_orig, ti_orig, num_frames = calculate_si_ti(
         cli_args.input,
         quiet=cli_args.quiet,
-        num_frames=cli_args.num_frames
+        num_frames=cli_args.num_frames,
+        width=cli_args.width,
+        height=cli_args.height,
     )
 
     si = np.round(np.array(si_orig).astype(float), 3).tolist()
@@ -182,7 +255,7 @@ def main():
             "input_file": [os.path.abspath(cli_args.input)] * num_frames,
             "si": si,
             "ti": ti,
-            "n": range(1, num_frames + 1)
+            "n": range(1, num_frames + 1),
         }
 
         writer.writerow(data.keys())
@@ -192,5 +265,5 @@ def main():
         raise RuntimeError("Wrong output format")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
