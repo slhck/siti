b76f15083533733b10ed60b4da83b4e8fe878005
Werner Robitza <werner.robitza@gmail.com>
2021-03-06 13:32:22 +0100

    format setup.py and switch to markdown

--- setup.py
+++ setup.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 from setuptools import setup, find_packages
+
 # To use a consistent encoding
 from codecs import open
 from os import path
@@ -8,55 +9,45 @@ from os import path
 here = path.abspath(path.dirname(__file__))
 
 # read version string
-with open(path.join(here, 'siti', '__init__.py')) as version_file:
+with open(path.join(here, "siti", "__init__.py")) as version_file:
     version = eval(version_file.read().split(" = ")[1].strip())
 
 # Get the long description from the README file
-with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+with open(path.join(here, "README.md"), encoding="utf-8") as f:
     long_description = f.read()
 
 # Get the history from the CHANGELOG file
-with open(path.join(here, 'CHANGELOG.md'), encoding='utf-8') as f:
+with open(path.join(here, "CHANGELOG.md"), encoding="utf-8") as f:
     history = f.read()
 
-try:
-    import pypandoc
-    long_description = pypandoc.convert_text(long_description, 'rst', format='md')
-    history = pypandoc.convert_text(history, 'rst', format='md')
-except ImportError:
-    print("pypandoc module not found, could not convert Markdown to RST")
-
 setup(
-    name='siti',
+    name="siti",
     version=version,
     description="Calculate Spatial Information / Temporal Information according to ITU-T P.910",
-    long_description=long_description + '\n\n' + history,
+    long_description=long_description + "\n\n" + history,
+    long_description_content_type="text/markdown",
     author="Werner Robitza",
-    author_email='werner.robitza@gmail.com',
-    url='https://github.com/slhck/siti',
-    packages=['siti'],
+    author_email="werner.robitza@gmail.com",
+    url="https://github.com/slhck/siti",
+    packages=["siti"],
     include_package_data=True,
     install_requires=["scipy", "numpy", "av", "tqdm"],
     license="MIT",
     zip_safe=False,
     setup_requires=["pytest-runner"],
     tests_require=["pytest"],
-    keywords='video, spatial information, temporal information',
+    keywords="video, spatial information, temporal information",
     classifiers=[
-        'Development Status :: 4 - Beta',
-        'Intended Audience :: Developers',
-        'Topic :: Multimedia :: Video',
-        'License :: OSI Approved :: MIT License',
-        'Natural Language :: English',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8'
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "Topic :: Multimedia :: Video",
+        "License :: OSI Approved :: MIT License",
+        "Natural Language :: English",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
     ],
-    entry_points={
-        'console_scripts': [
-            'siti = siti.__main__:main'
-        ]
-    },
+    entry_points={"console_scripts": ["siti = siti.__main__:main"]},
 )
